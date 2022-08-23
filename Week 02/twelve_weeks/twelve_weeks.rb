8f046ed40a830b28110eadf4f650d07349c8f869
Boris Tsang <108874411+BorisYTTsang@users.noreply.github.com>
2022-08-23 22:22:11 +0100

    Add files via upload

---
+++ Week 02/twelve_weeks/twelve_weeks.rb
@@ -0,0 +1,11 @@
+def twelve_weeks
+    weeks = 12
+    days = weeks * 7
+    hours = days * 24
+    mins = hours * 60
+    secs = mins * 60
+
+    puts "There are #{secs} seconds in 12 weeks!"
+end
+
+twelve_weeks
