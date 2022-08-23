aa680d7db583c75318b34c3645f7b8734d096881
Boris Tsang <108874411+BorisYTTsang@users.noreply.github.com>
2022-08-23 22:19:41 +0100

    Add files via upload

---
+++ Week 02/tsang_namer/tsang_namer.rb
@@ -0,0 +1,12 @@
+def tsang_namer
+	n1 = "Boris"
+	n2 = "Felix"
+	n3 = "David"
+	n4 = "Flora"
+	n5 = "Alan"
+	n6 = "Stephen"
+	s = ", "
+	puts n1 + s + n2 + s + n3 + s + n4 + s + n5 + s + n6 + "."
+end
+
+tsang_namer
